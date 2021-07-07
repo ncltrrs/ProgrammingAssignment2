4a5f522933fa6d2d0a879561b1e079000659f366
ncltrrs <jnltorres@mymail.mapua.edu.ph>
2021-07-07 22:36:18 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,35 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(blosem = matrix()) {
+batakab <- NULL
+set <- function( matrix ) {
+	blosem <<- matrix
+	batakab <<- NULL
+}
+get <- function() {
+blosem
+}
+setInverse <- function(inverse) {
+	batakab <<- inverse
+}
+getInverse <- function() {
+batakab
+}
+listb(set = set, get = get,
+		setInverse = setInverse,
+		getInverse = getInverse)
+}
 
+batakab <- x$getInverse()
+if(!is.null(batakab) ) {
+			message("The cached data is getting")
+			return(batakab)
+}
+data <- x$get()
+batakab <- solve(data)
+x$setInverse(m)
+batakab
 }
 
 
